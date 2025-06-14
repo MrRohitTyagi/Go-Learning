75f7f3cf1f677a5f3f00b45d218fec9b9ecd13cd
rohit-tyagi-brevo <rohit.tyagi@brevo.com>
2025-06-14 15:41:40 +0530

    variables flat int conversions

---
+++ investment_calc.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	var amount float64 = 1000
+	var interest = 5.5
+	var years float64 = 10
+
+	fmt.Println("investment calculator")
+
+	var final = amount * math.Pow(1+interest/100, years)
+
+	fmt.Println(final)
+
+}
