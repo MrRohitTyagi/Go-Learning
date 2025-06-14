2325adc872cb0a092fd820fc89ccbdb9cb06016f
rohit-tyagi-brevo <rohit.tyagi@brevo.com>
2025-06-14 16:16:40 +0530

    user input through terminal

--- investment_calc.go
+++ investment_calc.go
@@ -7,14 +7,18 @@ import (
 
 func main() {
 
-	var amount float64 = 1000
+	var amount float64
 	interest := 5.5
-	var years float64 = 10
+	var years float64
+	const inflationRate = 2.0
+
+	// get user input from terminal and store in variables using fmt.scan()
+	fmt.Scan(&amount) // & is used to get the address of the variable and will be used to store the input value
+	fmt.Scan(&years)
 
 	fmt.Println("investment calculator")
 
 	var value = amount * math.Pow(1+interest/100, years)
-	const inflationRate = 2.0
 
 	var valueAfterInflation = value - (value / 100 * inflationRate * years)
 
