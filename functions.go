d3657186096fb072eef53b3340f7b34099330217
rohit-tyagi-brevo <rohit.tyagi@brevo.com>
2025-06-14 17:41:37 +0530

    functions with multiple return calues

--- functions.go
+++ functions.go
@@ -5,12 +5,22 @@ import "fmt"
 func main() {
 
 	var output = add(3, 5)
+	var sum, sub, multi, divi = withMultipleReturns(3, 5)
 
-	fmt.Print(output)
+	fmt.Printf(output, sum, sub, multi, divi)
 
 }
 
 func add(a int, b int) string {
 	// Sprint is used to format the string and return it
 	return fmt.Sprint("The addition is: ", a+b)
+}
+
+func withMultipleReturns(a int, b int) (int, int, int, int) {
+
+	sum := a + b
+	sub := a - b
+	mult := a * b
+	divi := a / b
+	return sum, sub, mult, divi
 }
