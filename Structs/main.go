83e8c77042b6dc9b6cb4f92f7afd23559fee3204
rohit-tyagi-brevo <rohit.tyagi@brevo.com>
2025-06-16 00:12:00 +0530

    structs pointers

--- Structs/main.go
+++ Structs/main.go
@@ -17,8 +17,9 @@ type user struct { // defining struct (JS object) type
 func main() {
 
 	first := input.SInput()
-	last := input.SInput()
-	age := input.NInput()
+	// last := input.SInput()
+	last := "Tyagi"
+	age := 21
 
 	var person = user{ // basically objects
 		first:     first,
@@ -26,7 +27,12 @@ func main() {
 		age:       age,
 		createdAt: time.Now(),
 	}
+	person.last = "yadav" // updating value of struct field
+
+	structPointer(&person)
+
 	structArgs(person)
+
 }
 
 func structArgs(person user) {
@@ -35,4 +41,11 @@ func structArgs(person user) {
 	fmt.Println("person =>", person.last)
 	fmt.Println("person =>", person.age)
 	fmt.Println("person =>", person.createdAt)
+}
+func structPointer(person *user) {
+
+	// this function takes a pointer to the user struct
+	// and update its key
+	person.age = 43
+
 }
