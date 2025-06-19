e9d4446da961e356e3c1593f3787990707cd7406
rohit-tyagi-brevo <rohit.tyagi@brevo.com>
2025-06-19 15:57:10 +0530

    array  with loops

--- Arrays/main.go
+++ Arrays/main.go
@@ -2,13 +2,26 @@ package main
 
 import "fmt"
 
+type arr [3]string
+
 func main() {
 
 	// Arrays are fixed-size sequences of elements of the same type.
 	//  var prices = [length of array]type{v1,v2,v3,...}
 	var prices = [3]int{100, 200, 300}
-	var fruits = [3]string{"apple", "banana", "cherry"}
+	var fruits = arr{"apple", "banana", "cherry"}
 	fmt.Println("prices =>", prices)
 	fmt.Println("fruits =>", fruits)
 
+	arrayLoops(fruits)
+}
+
+func arrayLoops(fruits arr) {
+	for i := 0; i < len(fruits); i++ {
+
+		f := fruits[i]
+
+		println(f)
+
+	}
 }
