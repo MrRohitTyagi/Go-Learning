b26b879d7237361cf34efd42945f415df463fd8e
rohit-tyagi-brevo <rohit.tyagi@brevo.com>
2025-06-19 22:35:48 +0530

    array  slices

--- Arrays/main.go
+++ Arrays/main.go
@@ -8,10 +8,19 @@ func main() {
 
 	// Arrays are fixed-size sequences of elements of the same type.
 	//  var prices = [length of array]type{v1,v2,v3,...}
-	var prices = [3]int{100, 200, 300}
+	var prices = [5]int{100, 200, 300, 400, 500}
 	var fruits = arr{"apple", "banana", "cherry"}
 	fmt.Println("prices =>", prices)
 	fmt.Println("fruits =>", fruits)
+
+	prices[4] = 40000 // change the value of the third element
+	fmt.Println("prices =>", prices)
+
+	// Slices
+
+	fmt.Println("index 1 to 3(excluded) =>", prices[1:3])
+	fmt.Println("index 1 to last  =>", prices[1:])
+	fmt.Println(" index 0 to 3rd(excluded) =>", prices[:3])
 
 	arrayLoops(fruits)
 }
